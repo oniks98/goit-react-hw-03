cababb3188bc411599d6928bfdfe04e562235dfd
Yurii <oniks9821@gmail.com>
2024-12-15 21:00:43 +0200

    2100

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,34 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useId } from 'react';
 import ContactForm from '../ContactForm/ContactForm';
-import Contact from '../Contact/Contact';
 import SearchBox from '../SearchBox/SearchBox';
 import ContactList from '../ContactList/ContactList';
 import css from './App.module.css';
+import initialContacts from '../../contacts.json';
 
 function App() {
+  const [contacts, setContacts] = useState(initialContacts);
+  const [search, setSearch] = useState('');
+
+  const addContact = newContact => {
+    setContacts(prevContacts => [...prevContacts, newContact]);
+  };
+
+  const deleteContact = contactId => {
+    setContacts(prevContacts => {
+      return prevContacts.filter(contact => contact.id !== contactId);
+    });
+  };
+
+  const visibleContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
-    <div>
+    <div className={css.container}>
       <h1>Phonebook</h1>
-      <ContactForm />
-      <SearchBox />
-      <ContactList />
-      <Contact />
+      <ContactForm onAdd={addContact} />
+      <SearchBox value={search} onSearch={setSearch} />
+      <ContactList contacts={visibleContacts} onDelete={deleteContact} />
     </div>
   );
 }
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,11 +1,77 @@
-import { Formik, Form } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { useId } from 'react';
+import * as Yup from 'yup';
+import { nanoid } from 'nanoid/non-secure';
 import css from './ContactForm.module.css';
 
-function ContactForm() {
+const ContactFormSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(3, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  phone: Yup.string()
+    .matches(
+      /^\d{3}-\d{2}-\d{2}$/,
+      'Phone number must be in the format XXX-XX-XX'
+    )
+    .required('Required'),
+});
+
+const initialValues = {
+  username: '',
+  phone: '',
+};
+
+function ContactForm({ onAdd }) {
+  const nameFieldId = useId();
+  const phoneFieldId = useId();
+
+  const handleSumbit = (values, actions) => {
+    onAdd({
+      id: nanoid(),
+      name: values.username,
+      number: values.phone,
+    });
+    console.log('Submitted values:', values);
+    actions.resetForm();
+  };
+
   return (
-    <Formik initialValues={{}} onSubmit={() => {}}>
-      <Form>
-        <button type="submit">Add contact</button>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={handleSumbit}
+      validationSchema={ContactFormSchema}
+    >
+      <Form className={css.form}>
+        <div>
+          <label className={css.label} htmlFor={nameFieldId}>
+            Name
+          </label>
+          <Field
+            className={css.field}
+            type="text"
+            name="username"
+            id={nameFieldId}
+          />
+          <ErrorMessage name="username" component="span" />
+        </div>
+
+        <div>
+          <label className={css.label} htmlFor={phoneFieldId}>
+            Number
+          </label>
+          <Field
+            className={css.field}
+            type="tel"
+            name="phone"
+            id={phoneFieldId}
+          />
+          <ErrorMessage name="phone" component="span" />
+        </div>
+
+        <button className={css.btn} type="submit">
+          Add contact
+        </button>
       </Form>
     </Formik>
   );
